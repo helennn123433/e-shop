9fd17ce43553a7fb3ab75a925b48bfd69083703f
helennn123433 <lenomeltashevich@gmail.com>
2024-10-15 18:19:56 +0300

    Очистка ветки main

--- backend/src/main/java/com/example/EShop/repositories/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.EShop.repositories;
-
-import com.example.EShop.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-
-}
--- frontend/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-   <NNotificationProvider>
-    <router-view />
-  </NNotificationProvider>
-</template>
-
-<script setup>
-import { NNotificationProvider } from 'naive-ui';
-</script>
